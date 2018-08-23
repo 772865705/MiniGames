290616a23bb348dc3c75efd74fe576bc7712d523
772865705 <772865705@qq.com>
2018-08-23 19:49:16 +0800

    feat:日常push

--- app/src/main/java/com/zy/mini_games/swipe/LogicHelper.kt
+++ app/src/main/java/com/zy/mini_games/swipe/LogicHelper.kt
@@ -1,8 +1,12 @@
 package com.zy.mini_games.swipe
 
+import java.util.*
+
 /**
  * Created by Zhaoyue on 2018/8/22 0022.
  */
 object LogicHelper {
+    fun randomMines(xlength:Int,ylength:Int,x:Int,y:Int,count:Int){
 
+    }
 }
