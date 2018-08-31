6a86fc0044971ce652f2a4465fea0d08bcab8c3e
772865705 <772865705@qq.com>
2018-08-31 18:46:23 +0800

    feat:增加生成随机数的逻辑

--- app/src/main/java/com/zy/mini_games/swipe/LogicHelper.kt
+++ app/src/main/java/com/zy/mini_games/swipe/LogicHelper.kt
@@ -1,12 +1,38 @@
 package com.zy.mini_games.swipe
 
+import com.zy.mini_games.utils.loge
 import java.util.*
+import kotlin.collections.ArrayList
 
 /**
  * Created by Zhaoyue on 2018/8/22 0022.
  */
 object LogicHelper {
-    fun randomMines(xlength:Int,ylength:Int,x:Int,y:Int,count:Int){
+    fun randomMines(xlength:Int,ylength:Int,x:Int,y:Int,count:Int): ArrayList<Int> {
+        var total = xlength*ylength-1
+        if (count >= total || count <= 0){
+            loge("invalid count")
+            throw IllegalArgumentException("invalid count"+count)
+        }
 
+        val randoms = arrayListOf(getIndexByXY(xlength, ylength, x, y))
+
+        while (randoms.size <= count + 1){
+            val next = Random().nextInt(xlength * ylength)
+            if (!randoms.contains(next)){
+                randoms.add(next)
+            }
+        }
+        return randoms
+    }
+
+    fun getIndexByXY(xlength:Int,ylength:Int,x:Int,y:Int):Int{
+        return x + y * xlength;
+    }
+
+    fun getXYbyIndex(xlength:Int,ylength:Int,index:Int):IntArray{
+        val x = index%ylength
+        val y = index/ylength
+        return intArrayOf(x,y)
     }
 }
